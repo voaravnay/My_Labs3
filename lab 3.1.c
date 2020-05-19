b8df38faa50c0d56a44848019d7a141a1e7e2ad4
voaravnay <64018040+voaravnay@users.noreply.github.com>
2020-05-19 12:42:31 +0500

    Update lab 3.1.c

--- lab 3.1.c
+++ lab 3.1.c
@@ -9,7 +9,7 @@
 
 int main(void) {
     setlocale(LC_ALL, "RUS");
-    
+
     FILE* input, * output;
     int** array;
 
@@ -21,15 +21,15 @@ int main(void) {
         printf("\nФайл \"input.txt\" открыт успешно.");
         printf("\nЗагрузка данных...\n");
     }
-        
-        
+
+
     input = fopen("input.txt", "r");
     int count = 0, v = 0;
     while (fscanf(input, "%i", &v) != EOF) {
         count++;
     }
     fclose(input);
-    
+
     printf("\nКоличество элементов в файле - \"%d\".", count);
 
     int b = 0, space = 0, n = 0;
@@ -44,47 +44,55 @@ int main(void) {
     } while (feof(input) == 0);
     space++;
     fclose(input);
-    
-    n = count / space;  
+
+    n = count / space;
     int m = space;
-    
+
     printf("\nКоличество строк в файле - \"%d\".", n);
-    
+
     input = fopen("input.txt", "r");
-//Кол-во строк
-   
+    //Кол-во строк
+
 
     array = (int**)malloc(n * sizeof(int*));
-// Выделение памяти
-    
+    // Выделение памяти
+
     int i = 0, j = 0, ans = 0;
     for (i = 0; i < n; i++) {
 
 
-        array[i] = (int*)malloc(m * sizeof(int));
+
+        if (n == m) {
+            array = (int*)malloc(m * sizeof(int));
+        }
         for (j = 0; j < m; j++) {
 
-            fscanf(input, "%d", &mas[i][j]);
+            fscanf(input, "%d", &array[i][j]);
             if (array[i][j] == 0)
                 ans++;
         }
+
     }
     fclose(input);
-// Ввод из файла
+    for (int i = 0; i < n; i++) {
+        free(array[i]);
+    }
+    free(array);
+    // Ввод из файла
     output = fopen("output.txt", "w");
 
-    
+
     printf("\n");
     for (i = 0; i < n; i++) {
 
-        for (j = 0; j < m; j++) { 
+        for (j = 0; j < m; j++) {
 
             fprintf(output, "%d ", array[i][j]);
         }
         if (i != m && j != n)
             fprintf(output, "\n");
     }
-// Вывод данных в файл
+    // Вывод данных в файл
     fprintf(output, "Количество нулей в файле - \"%d\".", ans);
     fprintf(output, "\nЕсли количество нулей - \"0\", \nТо возможно числовой ряд в файле \"input.txt\" записан без пробелов и только с переносом строки.(не корректно)");
     fclose(output);
